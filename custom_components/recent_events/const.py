f42fd0dcf132fd430c484a75caaacb5a6a45c489
chaosl1996 <76881956+chaosl1996@users.noreply.github.com>
2025-02-27 22:36:49 +0800

    Update const.py

--- custom_components/recent_events/const.py
+++ custom_components/recent_events/const.py
@@ -3,9 +3,4 @@ CONF_CALENDAR_ID = "calendar_id"
 CONF_EVENT_COUNT = "event_count"
 DEFAULT_EVENT_COUNT = 3
 
-# 错误类型
-ERRORS = {
-    "entity_not_found": "entity_not_found",
-    "invalid_count": "invalid_count",
-    "service_error": "service_error"
-}
+ATTRIBUTION = "Calendar event data provided by connected services"
