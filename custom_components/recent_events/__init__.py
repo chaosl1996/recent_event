434675a960e1c8528ae6a309b70e59211d753b74
chaosl1996 <76881956+chaosl1996@users.noreply.github.com>
2025-02-27 20:17:36 +0800

    Create __init__.py

---
+++ custom_components/recent_events/__init__.py
@@ -0,0 +1,14 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """配置条目初始化"""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """配置条目卸载"""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
